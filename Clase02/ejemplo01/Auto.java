37c04c0edbd31f29b42fd77ef18e63cce2dfa460
zavhier <zavhier@gmail.com>
2026-01-08 12:18:39 -0300

    Update Auto.java

--- Clase02/ejemplo01/Auto.java
+++ Clase02/ejemplo01/Auto.java
@@ -1,1 +1,11 @@
+public class Auto  {
 
+  private int velocidad;
+
+  public void setVelocidad(int velocidad){
+      if(velocidad >=0){  // la velocidad no puede ser negativa
+         this.velociadad = velocidad 
+      }
+  }
+  
+}
