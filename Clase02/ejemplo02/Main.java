a7d9c29c177d3146aef3a80f59cf19201e6d3a29
zavhier <zavhier@gmail.com>
2026-01-08 12:46:13 -0300

    Update Main.java

--- Clase02/ejemplo02/Main.java
+++ Clase02/ejemplo02/Main.java
@@ -1,1 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+       List<Persona> personas = new ArrayList<Persona>();
+       Persona personaUno = new Persona();
+       personaUno.setNombre("Pedro");
+       Persona personaDos = new Persona();
+       personaDos.setNombre("Nico");
 
+       personas.add(personaUno);
+       personas.add(personaDos);
+
+      
+    
+    }
+}
