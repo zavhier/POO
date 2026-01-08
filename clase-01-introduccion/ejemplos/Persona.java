d16fcce1035082916ea5fd719c734d2f1f3a7ab5
zavhier <zavhier@gmail.com>
2026-01-08 11:12:30 -0300

    Update Persona.java

--- clase-01-introduccion/ejemplos/Persona.java
+++ clase-01-introduccion/ejemplos/Persona.java
@@ -1,1 +1,14 @@
+// Clase Persona
+// Representa una persona del mundo real
 
+public class Persona {
+
+    // Atributos
+    String nombre;
+    int edad;
+
+    // Método
+    void presentarse() {
+        System.out.println("Hola, soy " + nombre + " y tengo " + edad + " años");
+    }
+}
