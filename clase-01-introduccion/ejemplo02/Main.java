aaeb178424ac71382c5ee52ba12697d496507cb8
zavhier <zavhier@gmail.com>
2026-01-08 11:35:55 -0300

    Update Main.java

--- clase-01-introduccion/ejemplo02/Main.java
+++ clase-01-introduccion/ejemplo02/Main.java
@@ -1,1 +1,13 @@
 
+public class Main {
+
+    public static void main(String[] args) {
+        AutoEncapsulado auto = new AutoEncapsulado();
+        
+        auto.setMarca("Toyota");
+        auto.setModelo("Corolla");
+        auto.setVelocidad(140);
+        
+        auto.mostrarDatos();
+        
+    }
