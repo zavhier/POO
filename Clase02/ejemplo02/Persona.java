dbe10d1b4fa776c2a663011bebdac5a2ecb35b3a
zavhier <zavhier@gmail.com>
2026-01-08 12:43:12 -0300

    Update Persona.java

--- Clase02/ejemplo02/Persona.java
+++ Clase02/ejemplo02/Persona.java
@@ -1,5 +1,11 @@
 public class Persona {
 
    private String nombre;
-  
+
+   public void setNombre(String nombre){
+        this.nombre = nombre
+   }
+   public String getNombre(){
+         return this.nombre
+   }
 }
