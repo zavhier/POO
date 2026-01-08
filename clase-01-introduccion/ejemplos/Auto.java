581a4df384c2a24b59d12f970ae9872a3e71a836
zavhier <zavhier@gmail.com>
2026-01-08 11:18:47 -0300

    Update Auto.java

--- clase-01-introduccion/ejemplos/Auto.java
+++ clase-01-introduccion/ejemplos/Auto.java
@@ -1,1 +1,18 @@
 
+// Clase Auto
+// Representa un auto del mundo real
+
+public class Auto {
+
+    // Atributos (características)
+    String marca;
+    String modelo;
+    int velocidad;
+
+    // Método (comportamiento)
+    void mostrarDatos() {
+        System.out.println("Marca: " + marca);
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Velocidad: " + velocidad + " km/h");
+    }
+}
