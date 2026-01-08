0243c6e1c62d3555e11c5a138fad7861ebd64f60
zavhier <zavhier@gmail.com>
2026-01-08 11:23:24 -0300

    Update Main.java

--- clase-01-introduccion/ejemplos/Main.java
+++ clase-01-introduccion/ejemplos/Main.java
@@ -1,1 +1,21 @@
 
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Crear una instancia de Persona
+        Persona persona1 = new Persona();
+        persona1.nombre = "Juan";
+        persona1.edad = 20;
+        persona1.presentarse();
+
+        System.out.println("--------------");
+
+        // Crear una instancia de Auto
+        Auto auto1 = new Auto();
+        auto1.marca = "Ford";
+        auto1.modelo = "Fiesta";
+        auto1.velocidad = 120;
+        auto1.mostrarDatos();
+    }
+}
