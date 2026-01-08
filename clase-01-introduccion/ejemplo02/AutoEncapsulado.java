7b18255999ddea210b3ec5011e4cf52ed63daa4b
zavhier <zavhier@gmail.com>
2026-01-08 11:34:08 -0300

    Update AutoEncapsulado.java

--- clase-01-introduccion/ejemplo02/AutoEncapsulado.java
+++ clase-01-introduccion/ejemplo02/AutoEncapsulado.java
@@ -1,1 +1,36 @@
+public class AutoEncapsulado {
 
+    private String marca;
+    private String modelo;
+    private int velocidad;
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public void setVelocidad(int velocidad) {
+        this.velocidad = velocidad;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public int getVelocidad() {
+        return velocidad;
+    }
+
+    public void mostrarDatos() {
+        System.out.println("Marca: " + marca);
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Velocidad: " + velocidad + " km/h");
+    }
+}
